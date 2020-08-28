13a8263839c41b1e17a16fe4903ba3d3b8dffd66
Cimeiesh <sannaedstrom@gmail.com>
2020-08-28 15:20:14 +0200

    Chess app

---
+++ Flag/Flag/FlagApp/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace FlagApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("░░░░░░░░░░░░░░░░░░" , Console.ForegroundColor 
+            = ConsoleColor.Blue);
+            Console.Write("░░░░░", Console.ForegroundColor
+            = ConsoleColor.Yellow);
+
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+            = ConsoleColor.Blue);
+
+            Console.WriteLine(" ");
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+           = ConsoleColor.Blue);
+            Console.Write("░░░░░", Console.ForegroundColor
+            = ConsoleColor.Yellow);
+
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+            = ConsoleColor.Blue);
+
+            Console.WriteLine(" ");
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+           = ConsoleColor.Blue);
+            Console.Write("░░░░░", Console.ForegroundColor
+            = ConsoleColor.Yellow);
+
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+            = ConsoleColor.Blue);
+            Console.WriteLine(" ");
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+           = ConsoleColor.Blue);
+            Console.Write("░░░░░", Console.ForegroundColor
+            = ConsoleColor.Yellow);
+
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+            = ConsoleColor.Blue);
+            Console.WriteLine(" ");
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+           = ConsoleColor.Blue);
+            Console.Write("░░░░░", Console.ForegroundColor
+            = ConsoleColor.Yellow);
+
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+            = ConsoleColor.Blue);
+
+            // Yellow part
+            Console.WriteLine(" ");
+            Console.Write("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+           = ConsoleColor.Yellow);
+            Console.WriteLine(" ");
+            Console.Write("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+           = ConsoleColor.Yellow);
+
+            //Blue & Yellow part
+
+            Console.WriteLine("");
+
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+            = ConsoleColor.Blue);
+            Console.Write("░░░░░", Console.ForegroundColor
+            = ConsoleColor.Yellow);
+
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+            = ConsoleColor.Blue);
+
+            Console.WriteLine(" ");
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+           = ConsoleColor.Blue);
+            Console.Write("░░░░░", Console.ForegroundColor
+            = ConsoleColor.Yellow);
+
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+            = ConsoleColor.Blue);
+
+            Console.WriteLine(" ");
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+           = ConsoleColor.Blue);
+            Console.Write("░░░░░", Console.ForegroundColor
+            = ConsoleColor.Yellow);
+
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+            = ConsoleColor.Blue);
+            Console.WriteLine(" ");
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+           = ConsoleColor.Blue);
+            Console.Write("░░░░░", Console.ForegroundColor
+            = ConsoleColor.Yellow);
+
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+            = ConsoleColor.Blue);
+            Console.WriteLine(" ");
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+           = ConsoleColor.Blue);
+            Console.Write("░░░░░", Console.ForegroundColor
+            = ConsoleColor.Yellow);
+
+            Console.Write("░░░░░░░░░░░░░░░░░░", Console.ForegroundColor
+            = ConsoleColor.Blue);
+
+
+        }
+    }
+}
---
+++ chess/Chess/ChessApp/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+
+namespace ChessApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         /*
+          * Min första lösning
+            // Rad 1
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+            
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+           
+            Console.Write("░");
+            Console.Write("░");
+
+            // Rad 2
+            Console.WriteLine(" ");
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            // Rad 3
+            Console.WriteLine(" ");
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            // Rad 4
+            Console.WriteLine(" ");
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            // Rad 5
+            Console.WriteLine(" ");
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            // Rad 6
+            Console.WriteLine(" ");
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            // Rad 7
+            Console.WriteLine(" ");
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            // Rad 8
+            Console.WriteLine(" ");
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+
+            Console.Write("░");
+            Console.Write("░");
+
+            Console.Write("▓");
+            Console.Write("▓");
+         */
+
+            for (var y = 0; y < 8; y++)
+            {
+                for (var x = 0; x < 16; x++)
+                {
+                    int characterIndex = (x / 2 + y) % 2;
+                    char character = characterIndex == 0 ? '░' : '▓';
+
+                    Console.Write(character);
+                }
+                Console.Write('\n');
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
